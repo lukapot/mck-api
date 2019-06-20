e690f08dd41189f63415acd8bd21a681c53da655
Lukasz Potapczuk <lukasz.potapczuk@gmail.com>
2019-06-20 14:47:56 +0200

    task (Config): Configured GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      installSubscriptionHandlers: true,
+      subscriptions: {
+        onConnect: (connectionParams, websocket, context) => {
+          console.log('subscriptions onConnect');
+          return true;
+        },
+      },
+      playground: true,
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.schema.ts'),
+        outputAs: 'class',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
