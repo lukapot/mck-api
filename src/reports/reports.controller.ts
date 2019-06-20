7770159ada42cd250f14432e51f124fe156035e4
Lukasz Potapczuk <lukasz.potapczuk@gmail.com>
2019-06-20 15:33:24 +0200

    task (Reports): Added API controller

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,6 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Controller, Param, Put, Request } from '@nestjs/common';
+import { ApiImplicitParam } from '@nestjs/swagger';
+import { Report } from 'graphql.schema';
+import { ReportsService } from './reports.service';
 
 @Controller()
 export class ReportsController {
+  constructor(private readonly reportsService: ReportsService) {}
 
+  @Put('report/:reportId')
+  @ApiImplicitParam({name: 'reportId', required: true})
+  async update(@Param('reportId') reportId: string, @Request() request): Promise<Report> {
+    if (!reportId) {
+      throw new BadRequestException('reportId is required');
+    }
+    const { ticketState } = request.body;
+    return this.reportsService.update({ id: reportId, state: ticketState });
+  }
 }
