97664bbe685d8a421433991ba359bf8f011d9811
Lukasz Potapczuk <lukasz.potapczuk@gmail.com>
2019-06-20 15:05:11 +0200

    task (Reports): Added reports schema

---
+++ src/graphql.schema.ts
@@ -0,0 +1,63 @@
+
+/** ------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+export enum ReferenceResourceType {
+    ARTICLE = "ARTICLE",
+    POST = "POST",
+    REPLY = "REPLY"
+}
+
+export enum ReportType {
+    INFRINGES_PROPERTY = "INFRINGES_PROPERTY",
+    SPAM = "SPAM",
+    VIOLATES_POLICIES = "VIOLATES_POLICIES"
+}
+
+export enum Source {
+    REPORT = "REPORT"
+}
+
+export enum State {
+    OPEN = "OPEN"
+}
+
+export class CreateReportInput {
+    name: string;
+}
+
+export class UpdateReportInput {
+    id: string;
+    sourceIdentityId?: string;
+}
+
+export class Payload {
+    source?: Source;
+    reportType?: ReportType;
+    message?: string;
+    reportId?: string;
+    referenceResourceId?: string;
+    referenceResourceType?: ReferenceResourceType;
+}
+
+export abstract class IQuery {
+    abstract report(id: string): Report | Promise<Report>;
+}
+
+export class Reference {
+    referenceId?: string;
+    referenceType?: Source;
+}
+
+export class Report {
+    id: string;
+    source?: Source;
+    sourceIdentityId?: string;
+    reference?: Reference;
+    state?: State;
+    payload?: Payload;
+    created?: string;
+}
