8a9aa458db6e4a2b97803ce8c3cf0195b961c8e3
Lukasz Potapczuk <lukasz.potapczuk@gmail.com>
2019-06-20 15:09:52 +0200

    task (Reports): Prepared reports service and resolver

--- src/reports/reports.resolvers.ts
+++ src/reports/reports.resolvers.ts
@@ -1,6 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Report } from 'graphql.schema';
+import { ReportsService } from './reports.service';
 
 @Resolver('Reports')
 export class ReportsResolvers {
 
+  constructor(
+    private readonly reportsService: ReportsService,
+    ) {
+
+  }
+
+  @Query('report')
+  async report(
+    @Args('id')
+    id: string,
+  ): Promise<Report> {
+    return this.reportsService.findOneById(id);
+  }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,6 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import * as database from '../../assets/reports.json';
+import { Report } from '../graphql.schema';
 
 @Injectable()
 export class ReportsService {
+  private reportsDatabase: any = database;
 
+  public async findOneById(id: string): Promise<Report> {
+    const report = this.reportsDatabase.find(r => r.id === id);
+    return report;
+  }
 }
